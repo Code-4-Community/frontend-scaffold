b18530b3e41d4c2276da1edceeb6a48fa0f329b1
Varun Thakkar <varunthakkar2013@gmail.com>
2021-08-22 18:18:07 -0400

    created onboarding page

---
+++ src/containers/onboarding/index.tsx
@@ -0,0 +1,139 @@
+import React, { useState } from 'react';
+import { Helmet } from 'react-helmet';
+import { Button, Card, Form, Input, Typography } from 'antd';
+import { ContentContainer } from '../../components';
+import styled from 'styled-components';
+import { values } from 'lodash';
+import { ResponseCard } from '../../components/onboarding';
+
+const { Title, Paragraph } = Typography;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  align-items: center;
+  width: 100%;
+`;
+
+const FormCard = styled(Card)`
+  display: flex;
+  width: 50%;
+  border-radius: 5px;
+`;
+
+const OnboardingPageTitle = styled(Title)`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+`;
+
+const FormInput = styled(Input)`
+  display: flex;
+  width: 80%;
+  border: 1px 1px black;
+`;
+
+const StyledButton = styled(Button)`
+  width: 30%;
+  margin-top: 16px;
+`;
+
+const ResponseContainer = styled.div`
+  width: 75%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  margin-top: 16px;
+`;
+
+const SuccessMessage = styled(Paragraph)`
+  color: green;
+`;
+
+const Onboarding: React.FC = () => {
+  const [posts, setPosts] = useState<any[]>([]);
+  const [formSuccess, setFormSuccess] = useState<boolean>(false);
+
+  const updatePosts = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => setPosts(json));
+  };
+
+  const onFinish = (values: any) => {
+    setFormSuccess(false);
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      body: JSON.stringify(values),
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+    setFormSuccess(true);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  return (
+    <>
+      <Container>
+        <OnboardingPageTitle>
+          Code4Community Frontend Onboarding Tutorial!
+        </OnboardingPageTitle>
+        <FormCard bodyStyle={{ width: '100%' }}>
+          <Title>Form!</Title>
+          <Paragraph>This is an example form card.</Paragraph>
+          <Form onFinish={onFinish} onFinishFailed={onFinishFailed}>
+            <Form.Item
+              label="Favorite color"
+              name="favoriteColor"
+              rules={[
+                {
+                  required: true,
+                  message: 'Just tell me your favorite color :)',
+                },
+              ]}
+            >
+              <FormInput />
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+          {formSuccess && <SuccessMessage>Success!</SuccessMessage>}
+        </FormCard>
+        <StyledButton onClick={updatePosts}>Get Posts</StyledButton>
+        <ResponseContainer>
+          {posts.map(function (object, i) {
+            return (
+              <ResponseCard
+                id={object.id}
+                key={i}
+                title={object.title}
+                body={object.body}
+                userId={object.userId}
+              />
+            );
+          })}
+        </ResponseContainer>
+      </Container>
+    </>
+  );
+};
+
+export default Onboarding;
